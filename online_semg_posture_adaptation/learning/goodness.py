7bf0f5c0bc7112bec06b0a49f1c481194ac3549b
MarcelloZanghieri2 <59026506+MarcelloZanghieri2@users.noreply.github.com>
2023-08-23 10:22:44 +0200

    Update goodness.py

--- online_semg_posture_adaptation/learning/goodness.py
+++ online_semg_posture_adaptation/learning/goodness.py
@@ -1,3 +1,23 @@
+"""
+    Author(s):
+    Marcello Zanghieri <marcello.zanghieri2@unibo.it>
+    
+    Copyright (C) 2023 University of Bologna and ETH Zurich
+    
+    Licensed under the GNU Lesser General Public License (LGPL), Version 2.1
+    (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+    
+        https://www.gnu.org/licenses/lgpl-2.1.txt
+    
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+
 from __future__ import annotations
 
 # non-torch imports
